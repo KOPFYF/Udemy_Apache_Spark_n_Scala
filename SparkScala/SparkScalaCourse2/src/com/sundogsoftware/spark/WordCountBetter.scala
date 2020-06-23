960b52a3c0f8d6b4e94456e2a40fefb4b089bedf
KOPFYF <fei8@purdue.edu>
2020-06-23 00:22:35 -0400

    section 2 finished

---
+++ SparkScala/SparkScalaCourse2/src/com/sundogsoftware/spark/WordCountBetter.scala
@@ -0,0 +1,42 @@
+package com.sundogsoftware.spark
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+
+/** Count up how many of each word occurs in a book, using regular expressions. */
+object WordCountBetter {
+ 
+  /** Our main function where the action happens */
+  def main(args: Array[String]) {
+   
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    
+     // Create a SparkContext using every core of the local machine
+    val sc = new SparkContext("local[*]", "WordCountBetter")   
+    
+    // Load each line of my book into an RDD
+    val input = sc.textFile("../book.txt")
+    
+    // Split using a regular expression that extracts words
+    val words = input.flatMap(x => x.split("\\W+"))
+    
+    // Normalize everything to lowercase
+    val lowercaseWords = words.map(x => x.toLowerCase())
+    
+    // Count of the occurrences of each word
+    val wordCounts = lowercaseWords.countByValue()
+    
+    // Sort
+    val sortedResults = wordCounts.toSeq.sortBy(_._2)
+    
+    // Sort by descending
+    val sortedResults2 = wordCounts.toSeq.sortWith(_._2>_._2)
+    
+    // Print the results
+    sortedResults2.foreach(println)
+  }
+  
+}
+
